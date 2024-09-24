c10dcf35feee8151c37c835db66a4a4180b59c7c
fatemnhm <fatemanhm@gmail.com>
2024-09-24 17:00:51 +0300

    until exercise 5

---
+++ script.js
@@ -0,0 +1,136 @@
+/*
+Exercise 1: maxOfTwoNumbers()
+
+In this exercise, create a function named maxOfTwoNumbers. 
+It should take two numbers as inputs and return the larger number. 
+If they're equal, return either one.
+
+Exercise 1 has been completed for you:
+*/
+
+const maxOfTwoNumbers = (x, y) => {
+    if (x >= y) {
+      return x;
+    } else {
+      return y;
+    }
+  }
+  
+  console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+
+
+  /*
+Exercise 2: isAdult()
+
+Write a function named isAdult. It should take an age (number) 
+and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+
+Example: isAdult(21) should return 'Adult'.
+
+Complete the exercise in the space below:
+*/
+console.log('================================');
+
+const isAdult=(age)=>{
+    if(age>=18){
+        return 'Adult';
+
+    }else{
+        return 'Minor';
+    }
+}
+
+
+
+console.log('Exercise 2 Result:', isAdult(21));
+
+
+console.log('================================');
+
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+
+const isCharAVowel=(letter)=>{
+    
+
+    switch(letter) {
+        case "a"||"e"||"i"||"o"||"u":
+          return true;
+          break;
+       
+        default:
+         return false;
+      }
+
+
+}
+
+console.log('Exercise 3 Result:', isCharAVowel("a"));
+
+console.log('================================');
+
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+
+const generateEmail=(name,domain)=>{
+    return `${name}@${domain}`;
+}
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+
+console.log('================================');
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+
+const greetUser=(name,timeOfTheDay)=>{
+    
+    switch(imeOfTheDay){
+        
+        case 'morning':
+            return`Good Morning ${name}!`;
+            break;
+
+        case 'afternoon':
+            return `Good Afternoon ${name}!`;
+            break;
+
+         default:
+            return`Good Evening ${name}!`;
+             break;
+    }
+
+
+    
+}
+
+
+
+console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
