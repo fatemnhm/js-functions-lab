398e09ef673c6d2f259edbcfb667711f40ac8c13
fatemnhm <fatemanhm@gmail.com>
2024-09-25 22:48:48 +0300

    DONE

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ Complete the exercise in the space below:
 
 const greetUser=(name,timeOfTheDay)=>{
     
-    switch(imeOfTheDay){
+    switch(timeOfTheDay){
         
         case 'morning':
             return`Good Morning ${name}!`;
@@ -134,3 +134,250 @@ const greetUser=(name,timeOfTheDay)=>{
 
 
 console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
+
+console.log('================================');
+
+
+/*
+Exercise 6: maxOfThree()
+
+Define a function, maxOfThree. It should accept three numbers 
+and return the largest among them.
+
+Example: maxOfThree(17, 4, 9) should return 17.
+
+Complete the exercise in the space below:
+*/
+
+
+const maxOfThree1=(firstNumber,secondNumber,thirdNumber)=>{
+
+    return Math.max(firstNumber,secondNumber,thirdNumber);
+
+
+}
+const maxOfThree=(firstNumber,secondNumber,thirdNumber)=>{
+
+    if(firstNumber>=secondNumber && firstNumber>=thirdNumber)
+    {return firstNumber;} 
+    else if(secondNumber>=firstNumber && secondNumber>=thirdNumber){return secondNumber; }
+    else{return thirdNumber;}
+
+}
+console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
+
+console.log('================================');
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+
+const calculateTip=( bill, percentage)=>{
+
+   let tip=bill*(percentage/100);
+   return tip;
+
+}
+
+
+
+console.log('Exercise 7 Result:', calculateTip(50, 20));
+console.log('================================');
+
+
+/*
+Exercise 8: convertTemperature()
+
+Write a function named convertTemperature. 
+It takes two arguments: a temperature and a string representing the 
+scale ('C' for Celsius, 'F' for Fahrenheit). 
+Convert the temperature to the other scale.
+
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+Complete the exercise in the space below:
+*/
+
+const convertTemperature=( temperature, scale)=>{
+
+
+
+    switch(scale){
+        
+        case 'C':
+            return temperature*(9/5)+32;
+            break;
+
+         default:
+            return (temperature-32)*5/9;
+             break;
+    }
+
+
+    
+ }
+ 
+
+
+console.log('Exercise 8 Result:', convertTemperature(32, "C"));
+
+console.log('================================');
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+
+
+const basicCalculator=(firstNumber,secondNumber,operation)=>{
+    
+    switch(operation){
+        
+        case "add":
+            return firstNumber+secondNumber;
+            break;
+
+        case "subtract":
+            return firstNumber-secondNumber;
+            break;
+
+            case "multiply":
+                return firstNumber*secondNumber;
+                break;
+    
+         default:
+            return firstNumber/secondNumber;
+             break;
+    }
+
+
+    
+}
+
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
+
+
+console.log('================================');
+
+/*
+Exercise 10: calculateGrade()
+
+Define a function called calculateGrade. 
+It should take a numerical score and return the corresponding letter 
+grade (A, B, C, D, F). 
+
+For example, 90 and above yields an 'A', 80-89 is a 'B', 
+and 70-79 is a 'C', 60-69 is a 'D' and anything lower than a 60 is an 'F'.
+
+Example: calculateGrade(100) should return A.
+
+Complete the exercise in the space below:
+*/
+const calculateGrade=(grade)=>{
+    
+
+        
+        if (grade>=90){
+
+            return 'A' ;
+        }
+
+        else if (grade>=80){
+            return 'B' ;
+        }
+           
+         else if (grade>=70){
+            return 'C' ;
+        }
+        else if (grade>=60){
+            return 'D' ;
+        }
+
+        else {
+            return 'F' ;
+        }
+
+          
+    
+    }
+
+
+    
+
+
+
+console.log('Exercise 10 Result:', calculateGrade(85));
+
+
+
+
+
+
+
+/*
+
+console.log('================================');
+Exercise 11: createUsername()
+
+Define a function called createUsername. 
+It should take a first name and a last name and return a username. 
+
+The username should be a combination of the following:
+- The first three letters of the first name.
+- The first three letters of the last name.
+- The total character count of the first and last name combined.
+
+Example: createUsername('Samantha', 'Green') should return 'SamGre12'.
+
+Complete the exercise in the space below:
+console.log('Exercise 11 Result:', createUsername("Samantha", "Green"));
+*/
+
+
+
+
+
+
+/*
+Exercise 12: numArgs()
+
+Challenge yourself with numArgs. 
+This function should return the count of arguments passed to it when called.
+
+Complete the exercise in the space below:
+
+
+
+console.log('================================');
+const numArgs=(arguments)=>{
+
+
+
+
+    
+ }
+
+console.log('Exercise 12 Result:', numArgs(1, 2, 3, 4));
+
+
+*/
+
+
